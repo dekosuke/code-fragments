f9c23c2c65cf4afa6f60423d597f6a242a4d6967
dekosuke <deco.elb@gmail.com>
2011-12-09 00:51:57 +0900

    added monad-transformer practice

---
+++ MonadTransformer/Transformers.hs
@@ -0,0 +1,120 @@
+module Transformers where
+
+--For practice,
+--Examples in "Monad Transformers Step by Step"
+--http://www.grabmueller.de/martin/www/pub/Transformers.pdf
+
+import Control.Monad.Identity
+import Control.Monad.Error
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Writer
+
+import Data.Maybe
+import qualified Data.Map as Map
+
+type Name = String      --string
+
+data Exp = Lit Integer  --integer constant
+         | Var Name     --variable
+         | Plus Exp Exp --addition operation
+         | Abs Name Exp --lambda function
+         | App Exp Exp  --function application
+         deriving (Show)
+
+data Value = IntVal Integer       --integer
+           | FunVal Env Name Exp  --higher order function
+           deriving (Show)
+
+type Env = Map.Map Name Value  --key-value correspondence
+
+eval0 :: Env -> Exp -> Value
+eval0 env (Lit i) = IntVal i
+eval0 env (Var n) =fromJust (Map.lookup n env)
+eval0 env (Plus e1 e2) = 
+  let IntVal i1 = eval0 env e1
+      IntVal i2 = eval0 env e2
+  in IntVal (i1+i2)
+eval0 env (Abs n e) = FunVal env n e
+eval0 env (App e1 e2) = 
+  let val1 = eval0 env e1
+      val2 = eval0 env e2
+  in case val1 of 
+    FunVal env' n body -> eval0 (Map.insert n val2 env') body
+
+type Eval1 a = Identity a
+runEval1 :: Eval1 a -> a
+runEval1 ev = runIdentity ev
+
+eval1 :: Monad m => Env -> Exp -> m Value
+eval1 env (Lit i) = return $ IntVal i
+eval1 env (Var n) = return $ fromJust (Map.lookup n env)
+eval1 env (Plus e1 e2) = do
+  IntVal i1 <- eval1 env e1
+  IntVal i2 <- eval1 env e2
+  return $ IntVal (i1+i2)
+eval1 env (Abs n e) = return $ FunVal env n e
+eval1 env (App e1 e2) = do
+  val1 <- eval1 env e1
+  val2 <- eval1 env e2
+  case val1 of 
+    FunVal env' n body -> eval1 (Map.insert n val2 env') body
+
+type Eval2 a = ErrorT String Identity a
+
+runEval2 :: Eval2 a -> Either String a
+runEval2 ev = runIdentity (runErrorT ev)
+
+eval2 :: Env -> Exp -> Eval2 Value
+eval2 env (Lit i) = return $ IntVal i
+eval2 env (Var n) = 
+  case Map.lookup n env of
+    Nothing -> throwError ("unbound variable: " ++ n)
+    Just val -> return val
+eval2 env (Plus e1 e2) = do
+  do IntVal i1 <- eval2 env e1
+     IntVal i2 <- eval2 env e2
+     return $ IntVal (i1 + i2)
+eval2 env (Abs n e) = return $ FunVal env n e
+eval2 env (App e1 e2) = do
+  FunVal env' n body <- eval2 env e1
+  val2 <- eval2 env e2
+  eval2 (Map.insert n val2 env') body
+
+type Eval3 a = ReaderT Env (ErrorT String Identity) a
+runEval3 :: Env -> Eval3 a -> Either String a
+runEval3 env ev = runIdentity (runErrorT (runReaderT ev env))
+eval3 :: Exp -> Eval3 Value
+eval3 (Lit i) = return $ IntVal i
+eval3 (Var n) = do
+  env <- ask
+  case Map.lookup n env of
+    Nothing -> throwError ("unbound variable: " ++ n)
+    Just val -> return val
+eval3 (Plus e1 e2) = do
+  do e1' <- eval3 e1
+     e2' <- eval3 e2
+     case (e1', e2') of
+       (IntVal i1, IntVal i2) ->
+         return $ IntVal (i1 + i2)
+       _  -> throwError "type error in addition"
+eval3 (Abs n e) = do
+  env <- ask
+  return $ FunVal env n e
+eval3 (App e1 e2) = do
+  val1 <- eval3 e1
+  val2 <- eval3 e2
+  case val1 of
+    FunVal env' n body -> 
+      local (const (Map.insert n val2 env'))
+        (eval3 body)
+    _ -> throwError "type error in application"
+
+exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+typeErrorExp = Plus (Lit 1) (Abs "x" (Var "x"))
+keyErrorExp = Var "x"
+
+--main = print $ eval0 Map.empty exampleExp
+--main = print $ runEval1 (eval1 Map.empty exampleExp)
+--main = print $ runEval2 (eval2 Map.empty exampleExp)
+main = print $ runEval3 Map.empty (eval3 exampleExp)
