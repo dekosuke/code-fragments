1b5727c037d2adefb5ceb9190d4b42fc622c0902
JUN komiyama <deco.elb@gmail.com>
2011-12-09 16:51:04 +0900

    added 2.4

--- MonadTransformer/Transformers.hs
+++ MonadTransformer/Transformers.hs
@@ -114,7 +114,45 @@ eval3 (App e1 e2) = do
 
 --2.4 Adding State
 type Eval4 a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st ev = runIdentity (runStateT (runErrorT (runReaderT ev env)) st)
 
+tick :: (Num s, MonadState s m) => m ()
+tick = do
+   st <- get
+   put (st + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i) = do 
+  tick 
+  return $ IntVal i
+eval4 (Var n) = do
+  tick
+  env <- ask
+  case Map.lookup n env of
+    Nothing -> throwError ("unbound variable: " ++ n)
+    Just val -> return val
+eval4 (Plus e1 e2) = do
+     tick
+     e1' <- eval4 e1
+     e2' <- eval4 e2
+     case (e1', e2') of
+       (IntVal i1, IntVal i2) ->
+         return $ IntVal (i1 + i2)
+       _  -> throwError "type error in addition"
+eval4 (Abs n e) = do
+  tick
+  env <- ask
+  return $ FunVal env n e
+eval4 (App e1 e2) = do
+  tick
+  val1 <- eval4 e1
+  val2 <- eval4 e2
+  case val1 of
+    FunVal env' n body -> 
+      local (const (Map.insert n val2 env'))
+        (eval4 body)
+    _ -> throwError "type error in application"
 
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 typeErrorExp = Plus (Lit 1) (Abs "x" (Var "x"))
@@ -123,4 +161,5 @@ keyErrorExp = Var "x"
 --main = print $ eval0 Map.empty exampleExp
 --main = print $ runEval1 (eval1 Map.empty exampleExp)
 --main = print $ runEval2 (eval2 Map.empty exampleExp)
-main = print $ runEval3 Map.empty (eval3 exampleExp)
+--main = print $ runEval3 Map.empty (eval3 exampleExp)
+main = print $ runEval4 Map.empty 0 (eval4 exampleExp)
